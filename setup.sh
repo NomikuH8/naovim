df699a687816ccd883c48ee7aeb9a497cb179ae7
joao-menna <joao.menna1907@gmail.com>
2024-05-26 15:34:06 -0300

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-config_directory="~/.config/nvim"
+config_directory=~/.config/nvim
 
-if [ -d config_directory ]; then
+if [ -d $config_directory ]; then
   echo "Configuration directory exists, renaming it"
-  mv config_directory ~/.config/nvim.bak
+  mv $config_directory ~/.config/nvim.bak
 fi
 
 echo "Creating configuration directory"
-mkdir config_directory
+mkdir -p $config_directory
 
 echo "Copying all files to the configuration directory"
-cp -r ./* config_directory
+cp -r ./* $config_directory/
 
 echo "Finished, enjoy Naovim! :D"
 

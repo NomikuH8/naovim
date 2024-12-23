8289dacfca6fd6650c6ddd5a8a76d4656e63af21
João Menna <joao.menna@docspider.com.br>
2024-12-23 14:03:52 -0300

    Changed nvim-cmp for blink.cmp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,27 +1,21 @@
 return {
   'neovim/nvim-lspconfig',
+  dependencies = { 'saghen/blink.cmp' },
 
-  config = function()
-    require("lspconfig").jsonls.setup({
-      settings = {
-        json = {
-          schemas = require("schemastore").json.schemas(),
-          validate = { enable = true },
-        },
-      },
-    })
+  -- example using `opts` for defining servers
+  opts = {
+    servers = {
+      lua_ls = {}
+    }
+  },
 
-    require("lspconfig").yamlls.setup({
-      settings = {
-        yaml = {
-          schemaStore = {
-            enable = false,
-            url = "",
-          },
-
-          schemas = require("schemastore").yaml.schemas(),
-        },
-      },
-    })
-  end,
+  config = function(_, opts)
+    local lspconfig = require('lspconfig')
+    for server, config in pairs(opts.servers) do
+      -- passing config.capabilities to blink.cmp merges with the capabilities in your
+      -- `opts[server].capabilities, if you've defined it
+      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
+  end
 }
